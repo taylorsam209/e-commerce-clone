fa5684b832e107c1084cc2b44f66d6558f1d40a5
Taylor Sam <taylorsam209@yahoo.com>
2018-02-26 20:09:14 -0800

    Created inital router files

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Landing extends Component{
+    constructor() {
+        super()
+
+        this.state = {
+            input: ''
+        }
+    }
+    render() {
+        return(
+            <div>
+                Landing Page
+            </div>
+        )
+    }
+}
+
+export default Landing;
