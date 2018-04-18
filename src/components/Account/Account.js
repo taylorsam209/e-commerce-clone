82e002fa17749eac13b8c30cae379a1ac5564ab0
Taylor Sam <taylorsam209@yahoo.com>
2018-04-18 13:18:09 -0600

    Fixed containers width

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,25 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import Nav from "../Nav/Nav";
 import "./Account.css";
 
-class Account extends Component{
-    constructor() {
-        super()
+class Account extends Component {
+  constructor() {
+    super();
 
-        this.state = {
-            input: ''
-        }
-    }
-    render() {
-        return(
-            <div>
-                <Nav />
-                <div id="Account">
-                Account Page
-                </div>
-            </div>
-        )
-    }
+    this.state = {
+      input: ""
+    };
+  }
+  render() {
+    return (
+      <div id="Account">
+        <Nav />
+        <div className="account-content">
+          Account Page
+          <p>I know what to do in this time of need</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Account;
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,32 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Nav from "../Nav/Nav";
 import "./Landing.css";
-import {getCurrentUser} from "../../ducks/reducer";
-import {connect} from 'react-redux';
+import { getCurrentUser } from "../../ducks/reducer";
+import { connect } from "react-redux";
 
 class Landing extends Component {
-    constructor() {
-        super()
+  constructor() {
+    super();
 
-        this.state = {
-            input: ''
-        }
-    }
+    this.state = {
+      input: ""
+    };
+  }
 
-    // componentWillMount() {
-    //     this.props.getCurrentUser();
-    // }
-    render() {
-        return (
-            <div>
-                <Nav />
-                <div id="Landing">
-                    <div className="banner-container">
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  // componentWillMount() {
+  //     this.props.getCurrentUser();
+  // }
+  render() {
+    return (
+      <div id="Landing">
+        <Nav />
+        <div className="landing-content">
+          <div className="banner-container" />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default connect(null, {getCurrentUser})(Landing);
+export default connect(null, { getCurrentUser })(Landing);
