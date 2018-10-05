9663499c470d6edde1db22a9c6df92333c360da4
Taylor Sam <taylorsam209@yahoo.com>
2018-10-04 18:33:53 -0700

    Changed banner img code in Landing

--- src/components/Pages/Landing/Landing.js
+++ src/components/Pages/Landing/Landing.js
@@ -3,22 +3,25 @@ import Nav from "../..//Nav/Nav";
 import "./Landing.css";
 import { getProducts, addToCart } from "../../../ducks/reducer";
 import { connect } from "react-redux";
-import Products from './Products'
+import Products from "./Products";
+import banner from "../../../assets/banner.jpg";
 
 class Landing extends Component {
-
-  componentWillMount(){
-    this.props.getProducts()
+  componentWillMount() {
+    this.props.getProducts();
   }
 
   render() {
-    console.log('reduct products', this.props)
+    console.log("reduct products", this.props);
     return (
       <div id="Landing">
         <Nav />
         <div className="landing-content">
-          <section className="banner-container" />
-          <Products products={this.props.products} addToCart={this.props.addToCart} />
+          <img className="banner" alt="banner" src={banner} />
+          <Products
+            products={this.props.products}
+            addToCart={this.props.addToCart}
+          />
         </div>
       </div>
     );
@@ -30,7 +33,12 @@ const mapStateToProps = state => {
     products: state.products,
     user: state.user,
     cart: state.cart
-  }
+  };
+};
+
+const actions = {
+  addToCart, 
+  getProducts
 }
 
-export default connect(mapStateToProps, { addToCart, getProducts })(Landing);
+export default connect(mapStateToProps, actions)(Landing);
